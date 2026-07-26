44a2b483534b54cd07f6b5339febd46f1324ff10
Jesus-dev087 <26150475@aguascalientes.tecnm.mx>
2026-07-26 16:51:28 -0600

    Add files via upload

---
+++ area.py
@@ -0,0 +1,5 @@
+#programa para calcular el area base de un rectangulo
+base = float(input("ingresa la base: "))
+altura = float(input("ingrese la altura: "))
+area = base * altura 
+print("El area del rectangulo es:", area)
---
+++ peso.py
@@ -0,0 +1,5 @@
+# programa para calcular el IMC
+peso = float(input("Peso (kg): "))
+altura = float(input("Altura (m): "))
+imc = peso / (altura ** 2)
+print("Su imc es:", imc)
